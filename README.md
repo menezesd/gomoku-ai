652066b98025210cafc604da59de59f4d2b50f47
Dean Menezes <dean.menezes@fortanix.com>
2024-02-09 17:16:43 -0600

    aoeu

---
+++ README.md
@@ -0,0 +1,17 @@
+Usage
+
+Run the game by executing the runGomoku.py script with the following command-line arguments:
+
+
+python runGomoku.py <board size> <search depth> <player (1 or 2)>
+
+    <board size>: The size of the game board (e.g., 15 for a 15x15 board).
+    <search depth>: The depth to which the Minimax algorithm should search for the best move.
+    <player>: Indicates whether the human player is player 1 or player 2 (1 for player 1, 2 for player 2).
+
+Gameplay
+
+    Upon running the script, the Gomoku game window will appear.
+    The game board is displayed, and the AI will make its move based on the specified depth of search.
+    You, as the human player, will then make your move by clicking on an empty cell on the board.
+    The game alternates between human and AI turns until one player wins or the game ends in a draw.
---
+++ priority.py
@@ -0,0 +1,113 @@
+import heapq
+import itertools
+
+class myPriorityQueue:
+    """
+    Class for a priority queue implemented with a max heap.
+    """
+
+    def __init__(self):
+        self.pq = []                         # list of entries arranged in a heap
+        self.entry_finder = {}               # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'      # placeholder for a removed task
+        self.counter = itertools.count()     # unique sequence count
+
+    def add_task(self, task, priority=0):
+        """
+        Add a new task or update the priority of an existing task.
+        
+        Args:
+            task: The task to be added or updated.
+            priority: The priority of the task.
+        """
+        pq = self.pq
+        entry_finder = self.entry_finder
+        counter = self.counter
+        
+        if task in entry_finder:
+            self.remove_task(task)
+        count = next(counter)
+        entry = [-priority, count, task]
+        entry_finder[task] = entry
+        heapq.heappush(pq, entry)
+
+    def remove_task(self, task):
+        """
+        Mark an existing task as REMOVED. Raise KeyError if not found.
+        
+        Args:
+            task: The task to be removed.
+        """
+        entry_finder = self.entry_finder
+        entry = entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop_task(self):
+        """
+        Remove and return the lowest priority task. Raise KeyError if empty.
+        
+        Returns:
+            The lowest priority task.
+        """
+        pq = self.pq
+        entry_finder = self.entry_finder
+        while pq:
+            priority, count, task = heapq.heappop(pq)
+            if task is not self.REMOVED:
+                del entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
+
+    def increase_priority(self, task, n):
+        """
+        Increase the priority of a task by n.
+        
+        Args:
+            task: The task whose priority needs to be increased.
+            n: The amount by which the priority should be increased.
+        """
+        entry_finder = self.entry_finder
+        if task not in entry_finder:
+            return
+        priority, count, task = entry_finder[task]
+        self.add_task(task, n - priority)  # funny because max heap
+
+    def sorted_list(self):
+        """
+        Return a sorted list of tasks.
+        
+        Returns:
+            A list of tasks sorted by priority.
+        """
+        pq = self.pq
+        REMOVED = self.REMOVED
+        l = heapq.nsmallest(len(pq), pq)
+        ret = []
+        for entry in l:
+            add = entry[2]
+            if add is not REMOVED:
+                ret.append(add)
+        return ret
+
+    def truncated_sorted_list(self, t):
+        """
+        Return a sorted list of tasks up to a given priority.
+        
+        Args:
+            t: The priority threshold.
+        
+        Returns:
+            A list of tasks sorted by priority up to the given threshold.
+        """
+        pq = self.pq
+        REMOVED = self.REMOVED
+        l = heapq.nsmallest(len(pq), pq)
+        ret = []
+        for entry in l:
+            add = entry[2]
+            pri = entry[0]
+            if pri >= -t:
+                return ret
+            if add is not REMOVED:
+                ret.append(add)
+        return ret
